9cbca746066b123a6b02c8a0821b728e57343bd7
Roy Arsan <roy.arsan@gmail.com>
2014-09-15 13:15:43 -0700

    Splunk version bump to 6.1.3

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -2,10 +2,10 @@
 
 #Server
 default['splunk']['server_root']                  = "http://download.splunk.com/releases"
-default['splunk']['server_version']               = "6.0.1"
-default['splunk']['server_build']                 = "189883"
+default['splunk']['server_version']               = "6.1.3"
+default['splunk']['server_build']                 = "220630"
 
 #Forwarder
 default['splunk']['forwarder_root']               = "http://download.splunk.com/releases"
-default['splunk']['forwarder_version']            = "6.0.1"
-default['splunk']['forwarder_build']              = "189883"
+default['splunk']['forwarder_version']            = "6.1.3"
+default['splunk']['forwarder_build']              = "220630"
